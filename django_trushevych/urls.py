71c272f9da4a2160da0882572faf731a8d81e3fa
Andrii Trushevych <andrson451@gmail.com>
2021-09-30 22:18:16 +0300

    homework 17.1

--- django_trushevych/urls.py
+++ django_trushevych/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import include, path
 
 
 urlpatterns = [
+    path('project-debug/', include(debug_toolbar.urls)),
     path('', include('students.urls')),
     path('', include('group.urls')),
     path('', include('teachers.urls')),
--- students/views.py
+++ students/views.py
@@ -8,6 +8,12 @@ from .models import Student
 from .forms import StudentForm, GenerateRandomStudentForm, ContactUsForm
 from .tasks import create_random_students, send_email
 
+faker = Faker()
+
+
+def fake_phone_number(fake):
+    return f'+380{fake.msisdn()[4:]}'
+
 
 class IndexView(TemplateView):
     template_name = 'base/index.html'
@@ -27,36 +33,26 @@ class StudentListView(ListView):
 
 
 class GenerateStudentView(View):
-    faker = Faker()
-
-    @staticmethod
-    def fake_phone_number(fake):
-        return f'+380{fake.msisdn()[4:]}'
 
     def get(self, request, *args, **kwargs):
-        Student.objects.create(first_name=self.faker.first_name(),
-                               last_name=self.faker.last_name(),
-                               age=self.faker.random_int(18, 100),
-                               phone=self.fake_phone_number(self.faker))
+        Student.objects.create(first_name=faker.first_name(),
+                               last_name=faker.last_name(),
+                               age=faker.random_int(18, 100),
+                               phone=fake_phone_number(faker))
         return redirect('students')
 
 
 class GenerateStudentsView(View):
-    faker = Faker()
-
-    @staticmethod
-    def fake_phone_number(fake):
-        return f'+380{fake.msisdn()[4:]}'
 
     def get(self, request, *args, **kwargs):
         count = request.GET.get("count", "1")
         if count.isdigit():
             if 0 < int(count) <= 100:
                 for i in range(int(count)):
-                    Student.objects.create(first_name=self.faker.first_name(),
-                                           last_name=self.faker.last_name(),
-                                           age=self.faker.random_int(18, 100),
-                                           phone=self.fake_phone_number(self.faker))
+                    Student.objects.create(first_name=faker.first_name(),
+                                           last_name=faker.last_name(),
+                                           age=faker.random_int(18, 100),
+                                           phone=fake_phone_number(faker))
                 return redirect('students')
             else:
                 return HttpResponse("Count must be greater than 0, less or equal  than 100")
@@ -114,8 +110,8 @@ class SendingEmailView(FormView):
 
 
 def handler404(request, exception):
-    return render(request, './errors/404_error.html', status=404)
+    return render(request, 'errors/404_error.html', status=404)
 
 
 def handler500(request):
-    return render(request, './errors/500_error.html', status=500)
+    return render(request, 'errors/500_error.html', status=500)
