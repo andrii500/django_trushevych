d4aad683fb37241a8e984f1664dd9fb2718b741e
andrson <andrson451@gmail.com>
2021-10-12 11:26:56 +0300

    fix

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn django_trushevych.wsgi:application --log-file -
+ web: gunicorn django_trushevych.wsgi:application --log-file -
 worker: celery -A django_trushevych worker --beat --concurrency 10 -l info
--- django_trushevych/settings.py
+++ django_trushevych/settings.py
@@ -29,10 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv('DJANGO_DEBUG', True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ['*']
 
